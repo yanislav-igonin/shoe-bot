3fa3562a302daf43fcd213ed828e7cc84ff64e17
Yanislav Igonin <yanislavdabest@gmail.com>
2023-02-22 13:56:06 +0200

    1st  beta finished

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,34 @@
+/* eslint-disable require-unicode-regexp */
+import { config } from './config';
+import { getCompletion } from './openai';
 import { Bot } from 'grammy';
 
-// eslint-disable-next-line node/no-process-env
-const bot = new Bot(process.env.BOT_TOKEN ?? '');
+const bot = new Bot(config.botToken);
 
-bot.on(
-  'message:text',
-  async (context) => await context.reply('Echo: ' + context.message.text),
-);
+bot.on('message:text', async (context) => {
+  const { text } = context.message;
+  const fromId = context.message.from.id;
+  const notRightText =
+    text.startsWith('Ботинок,') || text.startsWith('ботинок,');
+  // Disable bot for other users for now
+  const notRightUser = fromId !== 142_166_671;
+  if (notRightText || notRightUser) {
+    return;
+  }
+
+  const rest = text.replace('Ботинок,', '').replace('ботинок,', '');
+
+  try {
+    const completition = await getCompletion(rest);
+    await context.reply(completition ?? 'LOL');
+  } catch (error) {
+    await context.reply('Что-то пошло не так');
+    throw error;
+  }
+});
+
+// eslint-disable-next-line no-console
+bot.catch(console.error);
 
 // eslint-disable-next-line no-console
 bot.start().catch(console.error);
---
+++ src/openai.ts
@@ -0,0 +1,17 @@
+import { config } from './config';
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: config.openAiApiKey,
+});
+const openai = new OpenAIApi(configuration);
+
+export const getCompletion = async (prompt: string) => {
+  const response = await openai.createCompletion({
+    max_tokens: 2_048,
+    model: 'text-davinci-003',
+    prompt,
+  });
+  const { text } = response.data.choices[0];
+  return text;
+};
