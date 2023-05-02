f853eb59e0c4227d4a413bd44b4ba0f91c7aa87b
Yanislav Igonin <yanislavdabest@gmail.com>
2023-05-02 21:41:45 +0300

    fix create dialog  method

--- src/repositories/dialog.repository.ts
+++ src/repositories/dialog.repository.ts
@@ -1,8 +1,6 @@
-import { type Dialog } from '@/database';
 import { database } from '@/database';
 
-export const create = async (data: Omit<Dialog, 'createdAt'>) =>
-  await database.dialog.create({ data });
+export const create = async () => await database.dialog.create({ data: {} });
 
 export const get = async (id: string) =>
   await database.dialog.findUnique({ where: { id } });
