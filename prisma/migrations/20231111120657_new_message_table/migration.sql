e67b900ad123eefe7de1d7478e951e5371f1f5fe
Yanislav Igonin <yanislavdabest@gmail.com>
2023-11-11 14:11:52 +0200

    add new table

---
+++ prisma/migrations/20231111120657_new_message_table/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "messages" (
+    "id" SERIAL NOT NULL,
+    "text" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "chatId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "tgPhotoId" TEXT,
+    "tgMessageId" TEXT NOT NULL,
+    "replyToId" INTEGER,
+
+    CONSTRAINT "messages_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "messages_tgMessageId_key" ON "messages"("tgMessageId");
+
+-- AddForeignKey
+ALTER TABLE "messages" ADD CONSTRAINT "messages_chatId_fkey" FOREIGN KEY ("chatId") REFERENCES "chats"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "messages" ADD CONSTRAINT "messages_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "messages" ADD CONSTRAINT "messages_replyToId_fkey" FOREIGN KEY ("replyToId") REFERENCES "messages"("id") ON DELETE SET NULL ON UPDATE CASCADE;
