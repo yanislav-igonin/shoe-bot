a4e2ff47e1b89178c137124b7b6ca4bf5a96eb34
Yanislav Igonin <yanislavdabest@gmail.com>
2023-12-02 22:21:58 +0200

    leave some code

--- src/scripts/1.migrate-all-messages.ts
+++ src/scripts/1.migrate-all-messages.ts
@@ -1,5 +1,12 @@
 // eslint-disable-next-line canonical/filename-match-regex
-import { type NewUser } from '@prisma/client';
+import {
+  type ChatType,
+  type Message,
+  type MessageType,
+  type NewChat,
+  type NewDialog,
+  type NewUser,
+} from '@prisma/client';
 import { database } from 'lib/database';
 import { logger } from 'lib/logger';
 
@@ -35,6 +42,59 @@ const run = async () => {
   await database.newUser.createMany({
     data: newUsers,
   });
+
+  // Recreate chats
+  // const chats = await database.chat.findMany();
+  // const newChats = chats.map((chat) => {
+  //   const newChat: Omit<NewChat, 'id'> = {
+  //     createdAt: chat.createdAt,
+  //     name: chat.name,
+  //     tgId: chat.id,
+  //     type: chat.type as ChatType,
+  //   };
+  //   return newChat;
+  // });
+  // await database.newChat.createMany({
+  //   data: newChats,
+  // });
+
+  // // Recreate dialogs
+  // const dialogs = await database.dialog.findMany();
+  // const newDialogs = dialogs.map((dialog) => {
+  //   const newDialog: Omit<NewDialog, 'id'> = {
+  //     chatId: null,
+  //     createdAt: dialog.createdAt,
+  //     oldId: dialog.id,
+  //   };
+  //   return newDialog;
+  // });
+  // await database.newDialog.createMany({
+  //   data: newDialogs,
+  // });
+  // const createdNewDialogs = await database.newDialog.findMany();
+  // const oldIdDialogIdMap = createdNewDialogs.reduce((accumulator, dialog) => {
+  //   accumulator[dialog.oldId] = dialog.id;
+  //   return accumulator;
+  // }, {} as Record<string, number>);
+
+  // const userMessages = await database.prompt.findMany();
+  // const botMessages = await database.botReply.findMany();
+
+  // const messages = [...userMessages, ...botMessages];
+  // messages.sort((a, b) => {
+  //   return a.createdAt.getTime() - b.createdAt.getTime();
+  // });
+
+  // const newMessages = messages.map((message) => {
+  //   let type: MessageType;
+  //   let newMessage: Message;
+  //   // Prompt message
+  //   // if (message.text && message.reply) {
+  //   //   newMessage.createdAt = message.createdAt;
+  //   //   newMessage.dialogId = message.dialogId ? oldIdDialogIdMap[message.dialogId] : null;
+  //   //   newMessage.
+  //   // };
+  // });
 };
 
 run()
