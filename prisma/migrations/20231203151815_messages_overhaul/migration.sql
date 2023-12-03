9f9d51f334334fdb3742d375101b1f47b9b31e6f
Yanislav Igonin <yanislavdabest@gmail.com>
2023-12-03 17:21:24 +0200

    change migration

--- prisma/migrations/20231203151815_messages_overhaul/migration.sql
+++ prisma/migrations/20231203151815_messages_overhaul/migration.sql
@@ -1,37 +1,8 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `chatId` on the `messages` table. All the data in the column will be lost.
-  - The `dialogId` column on the `messages` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-  - Added the required column `type` to the `messages` table without a default value. This is not possible if the table is not empty.
-  - Changed the type of `userId` on the `messages` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
 -- CreateEnum
 CREATE TYPE "ChatType" AS ENUM ('private', 'group', 'supergroup', 'channel');
 
 -- CreateEnum
 CREATE TYPE "MessageType" AS ENUM ('text', 'image', 'voice');
-
--- DropForeignKey
-ALTER TABLE "messages" DROP CONSTRAINT "messages_chatId_fkey";
-
--- DropForeignKey
-ALTER TABLE "messages" DROP CONSTRAINT "messages_dialogId_fkey";
-
--- DropForeignKey
-ALTER TABLE "messages" DROP CONSTRAINT "messages_userId_fkey";
-
--- DropIndex
-DROP INDEX "messages_tgMessageId_key";
-
--- AlterTable
-ALTER TABLE "messages" DROP COLUMN "chatId",
-ADD COLUMN     "type" "MessageType" NOT NULL,
-DROP COLUMN "userId",
-ADD COLUMN     "userId" INTEGER NOT NULL,
-DROP COLUMN "dialogId",
-ADD COLUMN     "dialogId" INTEGER;
 
 -- CreateTable
 CREATE TABLE "new_users" (
@@ -67,6 +38,22 @@ CREATE TABLE "new_dialogs" (
     CONSTRAINT "new_dialogs_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "messages" (
+    "id" SERIAL NOT NULL,
+    "text" TEXT,
+    "type" "MessageType" NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "dialogId" INTEGER,
+    "tgPhotoId" TEXT,
+    "tgMessageId" TEXT NOT NULL,
+    "tgVoiceId" TEXT,
+    "replyToId" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "messages_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "new_users_tgId_key" ON "new_users"("tgId");
 
@@ -78,3 +65,6 @@ ALTER TABLE "messages" ADD CONSTRAINT "messages_userId_fkey" FOREIGN KEY ("userI
 
 -- AddForeignKey
 ALTER TABLE "messages" ADD CONSTRAINT "messages_dialogId_fkey" FOREIGN KEY ("dialogId") REFERENCES "new_dialogs"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "messages" ADD CONSTRAINT "messages_replyToId_fkey" FOREIGN KEY ("replyToId") REFERENCES "messages"("id") ON DELETE SET NULL ON UPDATE CASCADE;
