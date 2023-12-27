cd99778d2aa365c8d4ff45027a3efd4142b35597
Yanislav Igonin <yanislavdabest@gmail.com>
2023-12-27 15:20:54 +0200

    Lint

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,5 @@
 export * from './image.controller';
 export * from './shicture.controller';
-export * from './textTrigger.controller';
 export * from './stats.controller';
 export * from './text.controller';
+export * from './textTrigger.controller';
