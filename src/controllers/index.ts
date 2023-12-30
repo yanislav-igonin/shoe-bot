1fbb7069b3f2568fdabcb0f07c16279797b54979
Yanislav Igonin <yanislavdabest@gmail.com>
2023-12-30 17:27:17 +0200

    move file

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,6 +1,6 @@
 export * from './admin/generate.controller';
+export * from './admin/stats.controller';
 export * from './image.controller';
 export * from './shicture.controller';
-export * from './stats.controller';
 export * from './text.controller';
 export * from './textTrigger.controller';
