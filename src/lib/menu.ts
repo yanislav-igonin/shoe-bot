14fe78c6179996d0418fb37184079ac1d57d3b22
Yanislav Igonin <yanislavdabest@gmail.com>
2024-01-05 14:26:07 +0200

    clean

--- src/lib/menu.ts
+++
@@ -1,6 +0,0 @@
-import { Menu } from '@grammyjs/menu';
-
-const menu = new Menu('my-menu-identifier')
-  .text('A', (context) => context.reply('You pressed A!'))
-  .row()
-  .text('B', (context) => context.reply('You pressed B!'));
